02e4f38f0ba7a5f66a1f79b25bad7f49116ab064
Wen Xiao <wennstyle@gmail.com>
2022-06-09 22:41:00 -0400

    commit tree 9c3b5d735a806f959dcf3d71b12bea49e71cbe5e

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+  attr_accessor :length, :width, :hypothenuese
+
+  def initialize(length, width, hypothenuese)
+    @length = length
+    @width = width
+    @hypothenuese = hypothenuese
+  end
+
+  def kind
+    validate_triangle
+    if length == width && width == hypothenuese
+      :equilateral
+    elsif length == width || width == hypothenuese || length == hypothenuese
+      :isosceles
+    else
+      :scalene
+    end
+
+  end
+
+  def valid_sides
+    [length, width, hypothenuese].all?(&:positive?)
+  end
+
+  def valid_triangle_inquality
+    length + width > hypothenuese && length + hypothenuese > width && width + hypothenuese > length
+  end
+
+  def validate_triangle
+    raise TriangleError unless valid_sides && valid_triangle_inquality
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
